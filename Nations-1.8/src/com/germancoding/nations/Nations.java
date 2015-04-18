b28011d04c404ea67e319d6a2b396e45ab5cd295
GermanCoding <Nummer378@aknm-craft.com>
2015-04-18 20:28:14 +0200

    Test

--- Nations-1.8/src/com/germancoding/nations/Nations.java
+++ Nations-1.8/src/com/germancoding/nations/Nations.java
@@ -19,7 +19,6 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitScheduler;
 import org.bukkit.scoreboard.ScoreboardManager;
 
